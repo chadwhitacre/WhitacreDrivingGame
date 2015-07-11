fdc5461aaf96961d5b8287f956d69d89edaca5b6
Chad Whitacre <chad@zetaweb.com>
2015-07-11 08:34:08 -0400

    Port to curses

--- WhitacreDrivingGame.py
+++ WhitacreDrivingGame.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import sys
+import curses
 import time
 
 
@@ -10,62 +10,99 @@ def double(val):
 def half(val):
     return val / 2
 
-class BadShiftTo(Exception):
-    pass
+COLORS = 'blue cyan green magenta red white yellow'.split()
+class BadColor(Exception): pass
+
 
 class Car(object):
 
-    position = 40
-    magnitude = 0
-    direction = 0
+    scr = None
+    position = (None, None)  # (y, x)
+    speed = 0
+    direction = (0, 1)
 
     def __init__(self, color, make, model, year):
+        if color not in COLORS:
+            raise BadColor()
         self.color = color
+        self._color = curses.color_pair(getattr(curses, 'COLOR_'+color.upper()))
         self.make = make
         self.model = model
         self.year = year
-
-    def shift_to(self, gear):
-        if gear == 'D':
-            self.direction = 1
-        elif gear == 'R':
-            self.direction = -1
-        elif gear == 'P':
-            self.direction = 0
-        else:
-            raise BadShiftTo
+        self.ymax, self.xmax = self.scr.getmaxyx()
+        self.position = (self.ymax // 2, self.xmax // 10)  # start at left middle
 
     def hit_gas(self):
-        if self.magnitude < 100:
-            self.magnitude += 1
+        if self.speed < 100:
+            self.speed += 1
 
     def hit_brake(self):
-        if self.magnitude > 0:
-            self.magnitude += -1
+        if self.speed > 0:
+            self.speed += -1
+
+    def steer(self):
+        raise NotImplementedError
 
     def drive(self):
-        print("Driving a {color} {year} {make} {model}! "
-              "Wheee!".format(**self.__dict__))
-        print('\n\n\n\n')
-        colorcode = { 'red': 41
-                    , 'green': 42
-                    , 'gray': 40
-                    , 'yellow': 43
-                    , 'blue': 46
-                     }[self.color]
+
+        self.scr.move(self.ymax-1, 0)
+        self.scr.addstr("Driving a ")
+        self.scr.addstr(self.color, self._color)
+        self.scr.addstr(" {year} {make} {model}! Wheee!".format(**self.__dict__))
+
+        #colorcode = { 'red': 41
+        #            , 'green': 42
+        #            , 'gray': 40
+        #            , 'yellow': 43
+        #            , 'blue': 46
+        #             }[self.color]
+
         while 1:
-            self.position = self.position + (self.magnitude * self.direction)
-            output = (' ' * self.position)
-            output += '\033[{};1m \033[39;49m\r'.format(colorcode)
-            print(output, end='')
-            sys.stdout.flush()
-            time.sleep(1)
+            # Redraw the car.
+            y, x = self.position
+            self.scr.delch(y, x)
+            y += self.speed * self.direction[0]
+            x += self.speed * self.direction[1]
+            self.scr.addstr(y, x, '#', self._color)
+            self.scr.refresh()
+            self.position = (y, x)
+            time.sleep(0.5)
 
 
-seths_car = Car('gray', 'Hyundai', 'Elantra', 2010)
-leahs_car = Car('green', 'Jeep', 'Wrangler', 2015)
-sams_car = Car('orange', 'Hummer', 'H2', 2015)
+def main(stdscr):
 
-leahs_car.shift_to('D')
-leahs_car.hit_gas()
-leahs_car.drive()
+    # wrapper doesn't actually initialize colors fully
+    curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i, i, -1)
+
+    # make cursor invisible
+    curses.curs_set(0)
+
+    # clear the screen
+    stdscr.clear()
+    stdscr.refresh()
+
+    Car.scr = stdscr
+
+    seths_car = Car('blue', 'Hyundai', 'Elantra', 2010)
+    leahs_car = Car('green', 'Jeep', 'Wrangler', 2015)
+    sams_car = Car('yellow', 'Hummer', 'H2', 2015)
+
+    leahs_car.hit_gas()
+    leahs_car.drive()
+
+
+def ctrl_c_wrapper(stdscr):
+    try:
+        main(stdscr)
+    except KeyboardInterrupt:
+        stdscr.clear()
+        y, x = stdscr.getmaxyx()
+        stdscr.move(y//2, (x//2)-2)
+        stdscr.addstr('Bye!', curses.color_pair(curses.COLOR_GREEN))
+        stdscr.refresh()
+
+
+if __name__ == '__main__':
+    curses.wrapper(ctrl_c_wrapper)
